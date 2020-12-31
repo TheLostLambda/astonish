231213c893f458ad33932dd82d7f0eeb2c1b17ec
Brooks J Rady <b.j.rady@gmail.com>
2020-12-31 19:18:53 +0000

    A little more work

--- src/main.lisp
+++ src/main.lisp
@@ -5,19 +5,37 @@
 
 ;;; Public Functions -----------------------------------------------------------
 
-(defun load-forms-from-file (file)
+(defun load-forms-from-file (file &optional package)
   "Read forms from a lisp file without any evaluation"
-  (let ((current-package *package*))
+  (let ((current-package (or package *package*)))
     (uiop:with-safe-io-syntax (:package current-package)
       (uiop:read-file-forms file))))
 
+;; FIXME: This should take a fancy query string that supports OR operations,
+;; filtering on cadr, caddr, etc, and should also work on atoms. Maybe something
+;; like this: "test . code-of-conduct > is-true, is-false", run on this:
+;;
+;; (TEST CODE-OF-CONDUCT "Is the given action unsuitable for the given pet?"
+;;   (IS-FALSE (PET "Cat")) (IS-FALSE (PET "Dog")) (IS-TRUE (PET "Fish"))
+;;   (IS-FALSE (PET "Rabbit")) (IS-FALSE (PET "Bird"))
+;;   (IS-TRUE (PLAY-FETCH "Cat")) (IS-FALSE (PLAY-FETCH "Dog"))
+;;   (IS-TRUE (PLAY-FETCH "Fish")) (IS-TRUE (PLAY-FETCH "Rabbit"))
+;;   (IS-TRUE (PLAY-FETCH "Bird"))))
+;;
+;; Should give:
+;;
+;; (IS-FALSE (PET "Cat")) (IS-FALSE (PET "Dog")) (IS-TRUE (PET "Fish"))
+;; (IS-FALSE (PET "Rabbit")) (IS-FALSE (PET "Bird"))
+;; (IS-TRUE (PLAY-FETCH "Cat")) (IS-FALSE (PLAY-FETCH "Dog"))
+;; (IS-TRUE (PLAY-FETCH "Fish")) (IS-TRUE (PLAY-FETCH "Rabbit"))
+;; (IS-TRUE (PLAY-FETCH "Bird"))))
 (defun select-conses (path forms)
   "Given a path (a list of car's) and list of forms, return all matching conses"
   (destructuring-bind (x &rest xs) path
     (let ((selected (filter-conses-by-car x forms)))
       (if (null xs)
           selected
-          (select-forms xs (apply #'append selected))))))
+          (select-conses xs (apply #'append selected))))))
 
 (defun macroexpand-select (macros form)
   "Recursively expand all occurrences of select macros"
--- tests/main.lisp
+++ tests/main.lisp
@@ -6,6 +6,25 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :astonish)' in your Lisp.
 
-(deftest test-target-1
-  (testing "should (= 1 1) to be true"
-    (ok (= 1 1))))
+(deftest load-forms-from-file-test
+  (testing "should be 10 top-level forms"
+    (ok (= 10 (length (load-forms-from-file #P"tests/data/pal-picker-test.lisp"))))))
+
+(deftest select-conses-test
+  (let ((all-forms (load-forms-from-file #P"tests/data/pal-picker-test.lisp" #.*package*)))
+    (testing "should work for nested forms"
+      (ok (equal '((PLAY-FETCH "Cat") (PLAY-FETCH "Fish")
+                   (PLAY-FETCH "Rabbit")(PLAY-FETCH "Bird"))
+                 (select-conses '(test is-true play-fetch) all-forms))))))
+
+(deftest macroexpand-select-test
+  (defmacro three (x) `(,x ,x ,x))
+  (defmacro rev (&body body) (reverse body))
+  (let ((test-form     '(defun wacky-stuff (maybe?)
+                          (if maybe? (loop :repeat 10 :collect '(three "cats"))
+                              (rev '(1 2 3) cadr))))
+        (expected-form '(defun wacky-stuff (maybe?)
+                          (if maybe? (loop :repeat 10 :collect '("cats" "cats" "cats"))
+                              (cadr '(1 2 3))))))
+    (testing "expands some, not all, macros"
+      (ok (equal expected-form (macroexpand-select '(three rev) test-form))))))
